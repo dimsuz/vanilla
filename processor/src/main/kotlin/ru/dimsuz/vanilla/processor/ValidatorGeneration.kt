353c8800156727756e587ee5f1b98fa0a3ac86b6
Dmitry Suzdalev <dimsuz@gmail.com>
2020-08-06 00:53:30 +0200

    Start work on rule methods generation

--- processor/src/main/kotlin/ru/dimsuz/vanilla/processor/ValidatorGeneration.kt
+++ processor/src/main/kotlin/ru/dimsuz/vanilla/processor/ValidatorGeneration.kt
@@ -2,6 +2,7 @@ package ru.dimsuz.vanilla.processor
 
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.TypeSpec
 import ru.dimsuz.vanilla.processor.either.Either
 import ru.dimsuz.vanilla.processor.file.writeFile
@@ -13,8 +14,22 @@ fun generateValidator(processingEnv: ProcessingEnvironment, mapping: PropertyMap
     .addType(
       TypeSpec
         .classBuilder(ClassName(mapping.models.sourcePackage, validatorClassName))
+        .addType(createBuilderTypeSpec(mapping))
         .build()
     )
     .build()
   return writeFile(processingEnv, fileSpec)
+}
+
+private fun createBuilderTypeSpec(mapping: PropertyMapping): TypeSpec {
+  return TypeSpec.classBuilder("Builder")
+    .addFunctions(createBuilderRuleFunctions(mapping))
+    .build()
+}
+
+private fun createBuilderRuleFunctions(mapping: PropertyMapping): Iterable<FunSpec> {
+  return mapping.mapping.keys.map { property ->
+    FunSpec.builder(property.name)
+      .build()
+  }
 }
--- processor/src/test/kotlin/ru/dimsuz/vanilla/processor/VanillaProcessorTest.kt
+++ processor/src/test/kotlin/ru/dimsuz/vanilla/processor/VanillaProcessorTest.kt
@@ -1,11 +1,15 @@
 package ru.dimsuz.vanilla.processor
 
 import com.google.common.truth.Truth.assertThat
+import com.squareup.kotlinpoet.metadata.ImmutableKmValueParameter
 import com.squareup.kotlinpoet.metadata.KotlinPoetMetadataPreview
 import com.squareup.kotlinpoet.metadata.toImmutableKmClass
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.SourceFile
 import com.tschuchort.compiletesting.SourceFile.Companion.kotlin
+import kotlinx.metadata.ClassName
+import kotlinx.metadata.KmClassifier
+import kotlinx.metadata.KmValueParameter
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
@@ -49,18 +53,66 @@ class VanillaProcessorTest {
 
     assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
 
-    val validatorClass = result.classLoader.loadClass("DraftValidator").toImmutableKmClass()
+    val validatorClass = result.classLoader.loadClass("DraftValidator\$Builder").toImmutableKmClass()
     assertThat(validatorClass.functions.map { it.name })
       .containsExactly("firstName", "lastName")
   }
 
-  // TODO matches properties with same name but different type
+  @Test
+  fun matchesPropertiesDifferingInType() {
+    val result = compile(kotlin("source.kt",
+      """
+        import ru.dimsuz.vanilla.annotation.ValidatedAs
+
+        @ValidatedAs(Validated::class)
+        data class Draft(val firstName: Int, val isAdult: String)
+        data class Validated(val firstName: Float, val isAdult: Boolean)
+      """.trimIndent()
+    ))
+
+    assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+
+    val validatorClass = result.classLoader.loadClass("DraftValidator\$Builder").toImmutableKmClass()
+    assertThat(validatorClass.functions.map { it.name })
+      .containsExactly("firstName", "isAdult")
+  }
+
+  @Test
+  fun generatesCorrectPropertyRuleSignature() {
+    val result = compile(kotlin("source.kt",
+      """
+        import ru.dimsuz.vanilla.annotation.ValidatedAs
+
+        @ValidatedAs(Validated::class)
+        data class Draft(val firstName: Int)
+        data class Validated(val firstName: Float)
+      """.trimIndent()
+    ))
+
+    assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+
+    val validatorClass = result.classLoader.loadClass("DraftValidator\$Builder").toImmutableKmClass()
+
+    assertThat(validatorClass.functions.single().valueParameters).hasSize(1)
+    val parameter = validatorClass.functions.first().valueParameters.first()
+    assertThat(parameter.name).isEqualTo("firstName")
+    assertThat(parameter.type?.classifier)
+      .isEqualTo(KmClassifier.Class("Validator"))
+    assertThat(parameter.type?.arguments?.map { it.type?.classifier })
+      .containsExactly(KmClassifier.Class("Int"), KmClassifier.Class("Float"), KmClassifier.TypeParameter(id = 0))
+  }
+
+  // TODO builder has Input, Output, Error type parameters
+  // TODO nullable validator signature in source, target
+  // TODO custom data classes in validator signature in source, target
+  // TODO works ok if target has more properties than source
+  // TODO gives an error if source and target have properties but no match can be found
   // TODO generates property validator signature which has target field type if it differs from source
 
   private fun prepareCompilation(vararg sourceFiles: SourceFile): KotlinCompilation {
     return KotlinCompilation()
       .apply {
-        workingDir = temporaryFolder.root,
+        workingDir = temporaryFolder.root
         annotationProcessors = listOf(VanillaProcessor())
         inheritClassPath = true
         sources = sourceFiles.asList()
