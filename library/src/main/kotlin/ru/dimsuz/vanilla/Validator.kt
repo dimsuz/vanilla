297562ebaeb20be33864a0067ff75144e143a79f
Dmitry Suzdalev <dimsuz@gmail.com>
2020-08-23 23:05:10 +0200

    Add functional-style operators
    Not sure if they'll be needed, maybe will delete later

--- library/src/main/kotlin/ru/dimsuz/vanilla/Validator.kt
+++ library/src/main/kotlin/ru/dimsuz/vanilla/Validator.kt
@@ -6,3 +6,10 @@ sealed class Result<out T, out E> {
   data class Ok<out T>(val value: T) : Result<T, Nothing>()
   data class Error<out E>(val first: E, val rest: List<E>? = null) : Result<Nothing, E>()
 }
+
+inline fun <T1, T2, E> Result<T1, E>.map(mapper: (T1) -> T2): Result<T2, E> {
+  return when (this) {
+    is Result.Ok -> Result.Ok(mapper(value))
+    is Result.Error -> this
+  }
+}
