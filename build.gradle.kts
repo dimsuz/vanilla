58d814a25f1d7357edb33fd590302d3bb3470f13
Dmitry Suzdalev <mail@dimsuz.ru>
2021-03-15 00:54:44 +0200

    Update Maven publishing config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 plugins {
   kotlin("jvm") version "1.4.31"
   `maven-publish`
+  signing
   id("org.jetbrains.dokka") version "1.4.20"
 }
 
 allprojects {
   repositories {
     mavenCentral()
+    jcenter()
   }
 }
 
@@ -45,6 +47,7 @@ subprojects {
   apply(plugin = "kotlin")
   apply(plugin = "maven-publish")
   apply(plugin = "org.jetbrains.dokka")
+  apply(plugin = "signing")
 
   tasks {
     compileKotlin {
@@ -84,8 +87,10 @@ subprojects {
           pom {
             val pomDescription: String by project
             val pomUrl: String by project
+            val pomName: String by project
             description.set(pomDescription)
             url.set(pomUrl)
+            name.set(pomName)
             scm {
               val pomScmUrl: String by project
               val pomScmConnection: String by project
@@ -112,6 +117,21 @@ subprojects {
                 name.set(pomDeveloperName)
               }
             }
+          }
+        }
+      }
+      signing {
+        sign(publishing.publications["maven"])
+      }
+      repositories {
+        maven {
+          val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+          val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+          val versionName: String by project
+          url = if (versionName.endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+          credentials {
+            username = project.property("NEXUS_USERNAME")?.toString()
+            password = project.property("NEXUS_PASSWORD")?.toString()
           }
         }
       }
