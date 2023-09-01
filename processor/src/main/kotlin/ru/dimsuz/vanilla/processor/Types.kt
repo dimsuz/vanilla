27285424d05a5deeaa6d96de5cecbc93397a7608
Ilya Nekrasov <ni@kode.ru>
2023-09-01 13:33:09 +0200

    Remove aggregated dependencies and fix incremental builds
    Send correct file spec for dependencies, before it was empty, because file haven't members

--- processor/src/main/kotlin/ru/dimsuz/vanilla/processor/Types.kt
+++ processor/src/main/kotlin/ru/dimsuz/vanilla/processor/Types.kt
@@ -1,11 +1,13 @@
 package ru.dimsuz.vanilla.processor
 
 import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSFile
 import com.squareup.kotlinpoet.PropertySpec
 
 typealias Error = String
 
 data class SourceAnalysisResult(
+  val file: KSFile,
   val models: ModelPair,
   val mapping: Map<PropertySpec, PropertySpec>,
   val unmappedTargetProperties: Set<PropertySpec>
